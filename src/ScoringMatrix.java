8b5d5f4ab4efe4c49d0cb8f979440b3bbbfe9d24
FarhatRV <abbasfarhat47@gmail.com>
2016-02-06 11:08:47 +0530

    Minor changes

--- src/ScoringMatrix.java
+++ src/ScoringMatrix.java
@@ -9,68 +9,78 @@ import java.util.Arrays;
  * Created by Kaancha on 30-01-2016.
  */
 public class ScoringMatrix {
-    public static int allCharacterCount[] = new int[128];
-    public static int allCharacterPairCount[][] = new int[128][128];
-    public static int scoringMatrix[][] = new int[128][128];
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        HashInput(args[0]);
-    }
+	public static int allCharacterCount[] = new int[128];
+	public static int allCharacterPairCount[][] = new int[128][128];
+	public static int scoringMatrix[][] = new int[128][128];
+	public static int bigramCountArray[];
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		HashInput(args[0]);
+		CountBigramFrequencies(10);
 
-    private static void BigramProbability(int countThreshold){
-        for(int i=0;i<128;i++){
-            for(int j=0;j<128;j++){
-                if(allCharacterPairCount[i][j]<countThreshold){
+	}
 
-                }
-            }
-        }
+/*
+* This function is for Nc.
+* */
+	private static void BigramProbability(int countThreshold){
+		for(int i=0;i<128;i++){
+			for(int j=0;j<128;j++){
+				if(allCharacterPairCount[i][j]<countThreshold){
 
-    }
+				}
+			}
+		}
 
-    private static void CountBigramFrequencies(int countThreshold){
-        int BigramFrequencies[] = new int[countThreshold];
-        for(int i=0;i<countThreshold;i++){
-            BigramFrequencies[i]=0;
-        }
-        for(int i=0;i<128;i++){
-            for(int j=0;j<128;j++){
+	}
 
-            }
-        }
-    }
+	/*
+	* This function is for smoothing.
+	* */
+	private static void CountBigramFrequencies(int countThreshold){
+		bigramCountArray = new int[countThreshold];
+		for(int i=0;i<countThreshold;i++){
+			bigramCountArray[i]=0;
+		}
+		for(int i=0;i<128;i++){
+			for(int j=0;j<128;j++){
+				if(allCharacterPairCount[i][j]<countThreshold)
+					bigramCountArray[allCharacterPairCount[i][j]]++;
+			}
+		}
+	}
 
-    private static void HashInput(String filename) throws UnsupportedEncodingException {
-        for(int i=0;i<128;i++){
-            allCharacterCount[i] = 0;
-        }
-        for(int i=0;i<128;i++){
-            for(int j=0;j<128;j++){
-                allCharacterPairCount[i][j]=0;
-            }
-        }
-        try {
-            Scanner scanner = new Scanner(new FileReader(filename));
-            while (scanner.hasNextLine()) {
-                String columns = scanner.nextLine();
-                if(columns.length() >0) {
-                    char last_char = columns.charAt(0);
-                    allCharacterCount[(int) columns.charAt(0)]++;
-                    for (int i = 1; i < columns.length(); i++) {
-                        allCharacterCount[(int) columns.charAt(i)]++;
-                        allCharacterPairCount[(int) last_char][columns.charAt(i)]++;
-                        last_char = columns.charAt(i);
-                    }
-                }
-            }
-            System.out.println(Arrays.toString(allCharacterCount));
-            //System.out.println(Arrays.deepToString(allCharacterPairCount));
-            PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
-            writer.println(Arrays.deepToString(allCharacterPairCount));
-            //writer.println("The second line");
-            writer.close();
-        } catch (FileNotFoundException e) {
-            System.out.println(e);
-        }
+	private static void HashInput(String filename) throws UnsupportedEncodingException {
+		for(int i=0;i<128;i++){
+			allCharacterCount[i] = 0;
+		}
+		for(int i=0;i<128;i++){
+			for(int j=0;j<128;j++){
+				allCharacterPairCount[i][j]=0;
+			}
+		}
+		try {
+			Scanner scanner = new Scanner(new FileReader(filename));
+			while (scanner.hasNextLine()) {
+				String columns = scanner.nextLine();
+				if(columns.length() >0) {
+					char last_char = columns.charAt(0);
+					allCharacterCount[(int) columns.charAt(0)]++;
+					for (int i = 1; i < columns.length(); i++) {
+						allCharacterCount[(int) columns.charAt(i)]++;
+						allCharacterPairCount[(int) last_char][columns.charAt(i)]++;
+						last_char = columns.charAt(i);
+					}
+				}
+			}
+			System.out.println(Arrays.toString(allCharacterCount));
+			//System.out.println(Arrays.deepToString(allCharacterPairCount));
+			PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
+			writer.println(Arrays.deepToString(allCharacterPairCount));
+			//writer.println("The second line");
+			writer.close();
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
 
-    }
+	}
 }
