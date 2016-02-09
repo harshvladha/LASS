03008e5fa4603d3c2137545d2c438f55a5dcf3a5
SNT93 <sudeshnt93@gmail.com>
2016-02-09 01:20:52 -0800

    before pulling

--- src/ScoringMatrix.java
+++ src/ScoringMatrix.java
@@ -11,8 +11,10 @@ import java.util.Arrays;
 public class ScoringMatrix {
 	public static int allCharacterCount[] = new int[128];
 	public static int allCharacterPairCount[][] = new int[128][128];
+    public static float modifiedAllCharacterPairCount[][] = new float[128][128];
 	public static int scoringMatrix[][] = new int[128][128];
-	public static int bigramCountArray[];
+	public static float bigramCountArray[];  //Nc
+    public static float modfiedBigramCountArray[];
 	public static void main(String[] args) throws UnsupportedEncodingException {
 		HashInput(args[0]);
 		CountBigramFrequencies(10);
@@ -37,16 +39,41 @@ public class ScoringMatrix {
 	* This function is for smoothing.
 	* */
 	private static void CountBigramFrequencies(int countThreshold){
-		bigramCountArray = new int[countThreshold];
-		for(int i=0;i<countThreshold;i++){
+		bigramCountArray = new float[countThreshold+2]; // +2 for 0 , 1 to k , k+1
+		for(int i=0;i<countThreshold+2;i++){
 			bigramCountArray[i]=0;
 		}
 		for(int i=0;i<128;i++){
 			for(int j=0;j<128;j++){
-				if(allCharacterPairCount[i][j]<countThreshold)
+				if(allCharacterPairCount[i][j]<countThreshold+2)
 					bigramCountArray[allCharacterPairCount[i][j]]++;
 			}
-		}
+        }
+        System.out.println(Arrays.toString(bigramCountArray));
+
+        //Smoothing Bigram Count Array
+        float N1 = bigramCountArray[1];
+        for(int c=0;c<bigramCountArray.length-1;c++)
+        {
+            if(bigramCountArray[c]!=0)
+            {
+                float x = (c+1)*bigramCountArray[c+1]/bigramCountArray[c];
+                float y =  (countThreshold+1)*bigramCountArray[countThreshold+1]/N1;
+                bigramCountArray[c] = ((x-c*y)/(1-y));
+            }
+        }
+
+        System.out.println(Arrays.toString(bigramCountArray));
+
+        // modifying the allCharacterPairCount Matrix
+        for(int i=0;i<128;i++){
+            for(int j=0;j<128;j++){
+                if(allCharacterPairCount[i][j]<=countThreshold) {
+                  //  allCharacterPairCount[i][j] =  bigramCountArray[allCharacterPairCount[i][j]];
+                }
+            }
+        }
+
 	}
 
 	private static void HashInput(String filename) throws UnsupportedEncodingException {
