61a3d01f00352653b67166a37a22b87bb6fb3f5d
Harsh Vardhan Ladha <harsh.ladha@gmail.com>
2016-03-16 15:53:15 +0530

    LASS code complete initial

--- src/LASS.java
+++ src/LASS.java
@@ -7,16 +7,32 @@ import java.util.Arrays;
 public class LASS {
 	public static double scoringMatrix[][] = new double[128][128];
 	public static String searchPattern; // pattern which we need to find
+	public static String LSS;
+	public static int LSSPos;
 	public static void main(String[] args) throws UnsupportedEncodingException {
 		/*
 		 * first argument is file/database to search in (args[0)
 		 * second argument is pattern which we have to find (args[1)
 		 * third argument is file of scoring matrix (args[2)
+		 * fourth argument is threshold (args[3)
 		 */
 		ImportScoringMatrix(args[2]);
 		String Seeds[] = new String[args[1].length()-1];
 		searchPattern = args[1];
 		Seeds = createSeeds(searchPattern, 3);
+		double score = 0;
+		double min_score = 1000000.00;
+		double threshold = Double.parseDouble(args[3]);
+		int j = 0;
+		for(int i=0;i<searchPattern.length()-2;i++){
+			score = ScoreOfEachSeed(Seeds[i]);
+			if(score > threshold && score < min_score){
+				min_score = score;
+				LSS = Seeds[i];
+				LSSPos = i;
+			}
+		}
+		exactmatchoflss(args[2], LSS, min_score);
 	}
 	private static String[] createSeeds(String pattern, int seedLength) {
 		String seeds[] = new String[pattern.length()-seedLength+1];
@@ -51,6 +67,7 @@ public class LASS {
         }
         return score;
 	}
+	/*
 	private static String leastscoringseeds(String lseed,int threshold){
 		if(ScoreOfEachSeed(lseed)>threshold){
 			return lseed;
@@ -58,33 +75,67 @@ public class LASS {
 		else
 			return "";
 	}
-	private static void exactmatchoflss(String filename,String lsseed,double seedscore){
+	*/
+	private static void exactmatchoflss(String filename, String lsseed, double seedscore){
 		try {
 			Scanner scanner = new Scanner(new FileReader(filename));
 			/* build scoring matrix from text file */
+			int lineNumber = 0, count = 0;
 			while (scanner.hasNextLine()) {
-				String columns = scanner.nextLine();
+				String line = scanner.nextLine();
+				lineNumber++;
 				int s=lsseed.length();
+				int L = line.length();				
 				double currentscore=0;
-				int i;
+				int i = 0;
+				int j = 1;
 				/*
 				 * seed length traversal should happen for all characters in this line.
 				 */
-					for (i=1;i<s;i++)
+				while(i < L){
+					while(j<s)
 					{
-						currentscore=currentscore+scoringMatrix[(int)columns.charAt(i-1)][(int)columns.charAt(i)];
+						currentscore=currentscore+scoringMatrix[(int)line.charAt(i+j-1)][(int)line.charAt(i+j)];
+						j++;
 					}
 					if(currentscore==seedscore){
 						/* 
-						 * Score matched, now we can extend left and right to make a EXACT match
+						 * Score matched, now we can extend left and right to make a EXACT match						 * 
 						 */
+						boolean foundFlag = true;
+						int k = i+j-s+1;
+						int i1=k, i2=k;
+						int lsspos1 = LSSPos;
+						int lsspos2 = LSSPos;
+						while(lsspos1 > 0 || lsspos2 < searchPattern.length()){
+							if(lsspos1 > 0 && searchPattern.charAt(lsspos1) == line.charAt(i1)){
+								lsspos1--;
+								i1--;
+							}
+							else if(lsspos2 < searchPattern.length() && searchPattern.charAt(lsspos2) == line.charAt(i2)){
+								lsspos2++;
+								i2++;
+							}
+							else{
+								foundFlag = false;
+								break;
+							}
+						}
+						if(foundFlag == true){
+							System.out.println("Pattern found at "+ (k - i1+1) + "Line : " + lineNumber);
+							count++;
+						}
 						
 					}
 					else{
-						currentscore=currentscore-scoringMatrix[(int)columns.charAt(i-s)][(int)columns.charAt(i-s+1)];
+						currentscore=currentscore-scoringMatrix[(int)line.charAt(i+j-s)][(int)line.charAt(i+j-s+1)];
 					}
+					i++;
+					j = s-1;
+				}
 
 			}
+			System.out.println("Total found instances : " + count);
 		} catch (FileNotFoundException e) {
 			System.out.println(e);
 		}
