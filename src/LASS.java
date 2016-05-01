2ab7941636db792dead086468503dff8623901e6
Harsh Vardhan Ladha <harsh.ladha@gmail.com>
2016-05-01 13:27:03 +0530

    developed and bug fixed #9

--- src/LASS.java
+++ src/LASS.java
@@ -30,14 +30,14 @@ public class LASS {
 		Seeds = createSeeds(searchPattern, 3);
 		//System.out.println(Seeds.length);
 		float score = 0;
-		float min_score = 0;
+		float min_score = 100000; // make it 0 for taking HSS 
 		float threshold = Float.parseFloat(args[3]);
 		int j = 0;
 		//System.out.println(searchPattern.length());
 		for(int i=0;i<searchPattern.length()-2;i++){
 			score = ScoreOfEachSeed(Seeds[i]);
 			System.out.println(Seeds[i] + " score : " + score);
-			if(score > threshold && score > min_score){
+			if(score > threshold && score < min_score){ // make => score > min_score for taking HSS
 				min_score = score;
 				LSS = Seeds[i];
 				LSSPos = i;
@@ -91,7 +91,7 @@ public class LASS {
 	private static void exactmatchoflss(String filename, String lsseed, float seedscore){
 		try {
 			Scanner scanner = new Scanner(new FileReader(filename));
-			/* build scoring matrix from text file */
+			/* scan document for the match */
 			int lineNumber = 0, count = 0, countLSS = 0; 
 			while (scanner.hasNextLine()) {
 				String line = scanner.nextLine();
@@ -118,41 +118,45 @@ public class LASS {
 					}
 					if((int)(currentscore*100000)==(int)(seedscore*100000)){
 						/* 
-						 * Score matched, now we can extend left and right to make a EXACT match						 * 
+						 * Score matched, now we can extend left and right to make an EXACT match						 
+						 * 
 						 */
 						
 						//System.out.println("LSS found at Line " + lineNumber );
 						countLSS++;
-						boolean foundFlag = true;
+						boolean foundFlag = false;
 						int k = i+j-s;
+						//System.out.println("value of K here is : " + k + " and LSS pos in search string is : " + LSSPos);
 						int i1=k, i2=k;
 						int lsspos1 = LSSPos;
 						int lsspos2 = LSSPos;
 						//System.out.println(LSSPos + " " + i + " " + j);
-						while((lsspos1 > 0 || lsspos2 < searchPattern.length()) && (i1 > 0 || i2 > 0)){
-							if((lsspos1 > 0 && searchPattern.charAt(lsspos1) == line.charAt(i1)) && i1 > 0 ){
+						while((lsspos1 > -1 || lsspos2 < searchPattern.length()) && (i1 > -1 || i2 < line.length())){
+							if((lsspos1 > -1 && i1 > -1 && searchPattern.charAt(lsspos1) == line.charAt(i1)) ){
 								lsspos1--;
 								i1--;
 							}
-							else if((lsspos2 < searchPattern.length() && searchPattern.charAt(lsspos2) == line.charAt(i2)) && i2 < line.length()){
+							else if((lsspos2 < searchPattern.length() && i2 < line.length() && searchPattern.charAt(lsspos2) == line.charAt(i2))){
 								lsspos2++;
 								i2++;
 							}
 							else{
-								foundFlag = false;
 								break;
+							}
+							if(lsspos1 == -1 && lsspos2 == searchPattern.length()){
+								foundFlag = true;
 							}
 						}
 						if(foundFlag == true){
-							System.out.println("Pattern found at index " + (i1+1) +  " of Line : " + lineNumber);
+							System.out.println("Pattern found at index " + (i1+2) +  " of Line : " + lineNumber);
 							count++;
 						}
 					}
 					currentscore=currentscore-scoringMatrix[(int)line.charAt(i+j-s)][(int)line.charAt(i+j-s+1)];
 					i++;
-					j = s-1;
+					j = s-1;	
 				}
-
+				//break;
 			}
 			System.out.println("Total found instances : " + count + " and LSS = " + countLSS + " LSS is " + LSS);
 		} catch (FileNotFoundException e) {
